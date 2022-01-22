ca20a90fd302e1a19f44a50214b8ff739d7bc3d7
FeiWhang <feiwhang@gmail.com>
2022-01-22 16:07:14 +0700

    create animal type mapping in factory

--- src/main/java/io/muic/ooc/fab/AnimalFactory.java
+++ src/main/java/io/muic/ooc/fab/AnimalFactory.java
@@ -1,15 +1,26 @@
 package io.muic.ooc.fab;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AnimalFactory {
 
+    private static final Map<AnimalType, Class<? extends Animal>> ANIMAL_TYPE_CLASS_MAP = new HashMap<>();
+
+    static {
+        ANIMAL_TYPE_CLASS_MAP.put(AnimalType.RABBIT, Rabbit.class);
+        ANIMAL_TYPE_CLASS_MAP.put(AnimalType.FOX, Fox.class);
+    }
 
     public static Animal createAnimal(AnimalType animalType,boolean randomAge ,Field field, Location location) {
-        if (animalType.equals(AnimalType.RABBIT)) {
-            return new Rabbit(randomAge, field, location);
-        } else if (animalType.equals(AnimalType.FOX)) {
-           return new Fox(randomAge, field, location);
-        } else {
-            throw new RuntimeException("Unknown animalType");
+
+        try {
+            return ANIMAL_TYPE_CLASS_MAP.get(animalType).getDeclaredConstructor(boolean.class, Field.class, Location.class).newInstance(randomAge, field, location);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
         }
+
+        throw new RuntimeException("Unknown animalType");
     }
 }
